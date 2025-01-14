f09856e0ee2cd2f195149f7594c42c8f25957f6a
Chloe Constantineau <chloe_constantineau@hotmail.com>
2025-01-13 21:36:40 -0500

    Modify and test Alphabet

--- src/test/scala/io/shortener/AlphabetSpec.scala
+++ src/test/scala/io/shortener/AlphabetSpec.scala
@@ -1,6 +1,6 @@
 package io.shortener
 
-import io.shortener.services.Alphabet
+import io.shortener.slug.Alphabet
 import zio.Scope
 import zio.test.*
 
@@ -8,24 +8,43 @@ object AlphabetSpec extends ZIOSpecDefault:
 
     def spec: Spec[TestEnvironment & Scope, Any] =
       suite("AlphabetSpec")(
-        test("Should have default alphabet when 'None' is provided") {
-          assertZIO(Alphabet.live(None))(Assertion.equalTo(Alphabet.default))
+        test("Should fallback to default when no alphabet is provided") {
+          val someAlphabet = Alphabet.live(None)
+          assertZIO(someAlphabet)(equalsFallback)
         },
         test("Should fallback to default when provided alphabet is too small") {
-          checkN(1)(Gen.alphaNumericStringBounded(0, Alphabet.minLength - 1)): a =>
-            assertZIO(Alphabet.live(Some(a)))(Assertion.equalTo(Alphabet.default))
+          check(Gen.alphaNumericStringBounded(0, Alphabet.MIN_LENGTH - 1)): a =>
+              val someAlphabet = Alphabet.live(Some(a))
+              assertZIO(someAlphabet)(equalsFallback)
         },
         test("Should fallback to default when provided alphabet has duplicate characters") {
-          val duplicates = (('a' to 'z') ++ ('a' to 'z')).toString()
-          assertZIO(Alphabet.live(Some(duplicates)))(Assertion.equalTo(Alphabet.default))
+          val duplicates   = (('a' to 'z') ++ ('a' to 'z')).toString()
+          val someAlphabet = Alphabet.live(Some(duplicates))
+          assertZIO(someAlphabet)(equalsFallback)
         },
         test("Should fallback to default when provided alphabet has multibyte characters") {
-          checkN(1)(Gen.stringBounded(Alphabet.minLength, 50)(Gen.unicodeChar)): a =>
-            assertZIO(Alphabet.live(Some(a)))(Assertion.equalTo(Alphabet.default))
+          check(Gen.stringBounded(Alphabet.MIN_LENGTH, 50)(Gen.unicodeChar)): a =>
+              val someAlphabet = Alphabet.live(Some(a))
+              assertZIO(someAlphabet)(equalsFallback)
         },
         test("Should succeed when valid alphabet value is provided") {
-          check(Gen.setOfBounded(Alphabet.minLength, 30)(Gen.asciiChar)): set =>
-              val a = set.foldLeft("")((acc, i) => acc + i.toString)
-              assertZIO(Alphabet.live(Some(a)))(Assertion.not(Assertion.equalTo(Alphabet.default)))
+          check(Gen.setOfBounded(Alphabet.MIN_LENGTH, 30)(Gen.asciiChar)): set =>
+              val someAlphabet = Alphabet.live(Some(set.foldLeft("")((acc, i) => acc + i.toString)))
+              assertZIO(someAlphabet)(notEqualToFallback)
+        },
+        test("Alphabet shuffle should be deterministic") {
+          assertTrue(alphabet.shuffle == alphabet.shuffle)
+        },
+        test("Should validate ids") {
+          val id = "aAbBcC"
+          assertTrue(alphabet.validId(id), !alphabet.validId(id + "invalid id!"))
+        },
+        test("Should generate valid ids") {
+          check(Gen.long): i =>
+              assertTrue(alphabet.validId(alphabet.toId(i)))
         },
       ) @@ TestAspect.silentLogging
+
+    private val alphabet: Alphabet = Alphabet.default
+    private val equalsFallback     = Assertion.equalTo(alphabet)
+    private val notEqualToFallback = Assertion.not(equalsFallback)
